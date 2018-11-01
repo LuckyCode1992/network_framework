4ffdc7ba6e09bad32bc178c70c8ca7ff14efabd0
2214576751@qq.com <hxl19920401>
2018-11-01 16:54:02 +0800

    优化代码

--- networkframework/src/main/java/com/justcode/hxl/networkframework/retrofit/BussinessResult.kt
+++ networkframework/src/main/java/com/justcode/hxl/networkframework/retrofit/BussinessResult.kt
@@ -1,15 +1,21 @@
 package com.justcode.hxl.networkframework.retrofit
 
+import com.google.gson.Gson
+
 /**
  * 参数根据自己的接口定义
  */
 open class BussinessResult<T>(var err_code: String? = null,
                               var err_code_des: String? = "",
+
                               var body: T? = null) : HttpResult<T> {
     override fun obtainIsSuccess(): Boolean = "0" == err_code
 
     override fun obtainData(): T? = body
 
-    override fun obtainMessage(): String? = err_code_des
+    override fun obtainMessage(): String {
+        return "$err_code&&$err_code_des"
+
+    }
 
 }
